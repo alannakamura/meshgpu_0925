97e31d1e410b59f6db85964ad592f01aa3e71311
alannakamura <alancosta1988@gmail.com>
2025-09-23 19:55:08 -0300

    210925

--- runMany_tests2.py
+++ runMany_tests2.py
@@ -3,7 +3,7 @@ from tqdm import tqdm
 import os
 import pycuda.driver as cuda
 
-problem = [34, 38, 314]
+problem = [34]
 #problem = [32,33,34,35,36,37,38,39,310,311,312,313,314]
 # problem = [11,12,13,14,16]
 # problem = [4,7,1,2,3,5,6]
