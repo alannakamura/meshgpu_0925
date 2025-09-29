fb94deaa63f7a7027f795e9430ee8fc94eb1a44e
alannakamura <alancosta1988@gmail.com>
2025-09-29 16:26:10 -0300

    290925

--- results2d.py
+++ results2d.py
@@ -9,7 +9,7 @@ from optimisationMap import *
 import pickle
 
 # name_file = 'results.p
-name_file = 'results_311_300sim_30iter_128pop_3posdim_3060.pkl'
+name_file = 'results_313_200sim_30iter_128pop_3posdim_5070.pkl'
 
 f = open(name_file, 'rb')
 results = pickle.load(f)
@@ -31,6 +31,8 @@ if name_problem == 'wfg1':
     pf_b = pickle.load(f)
     pf_a = problem.pareto_front()
     f.close()
+elif name_problem == 'mw13':
+    pf_a = np.zeros((1000,2))
 else:
     pf_a = problem.pareto_front()
 # pf_a = problem.pareto_front(use_cache=False)
