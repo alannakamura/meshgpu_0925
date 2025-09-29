b9ff289fc17a06fe5f9d473a4ca50e7c51c4f50e
alannakamura <alancosta1988@gmail.com>
2025-09-28 21:11:39 -0300

    280925

--- results2d.py
+++ results2d.py
@@ -9,7 +9,7 @@ from optimisationMap import *
 import pickle
 
 # name_file = 'results.p
-name_file = 'results_33_200sim_30iter_128pop_3posdim_3060.pkl'
+name_file = 'results_311_300sim_30iter_128pop_3posdim_3060.pkl'
 
 f = open(name_file, 'rb')
 results = pickle.load(f)
