24bb3efef605f2273dec6707b7f66f6d3abb412b
alan <alancosta1988@gmail.com>
2025-10-13 13:00:20 -0300

    131025

--- runMany_tests2.py
+++ runMany_tests2.py
@@ -5,9 +5,9 @@ import pycuda.driver as cuda
 
 # problem = [311]
 #problem = [31,32,33,34,35,36,37,38,39,310,311,312,313,314]
-# problem = [11,12,13,14,16]
+problem = [11,12,13,14,16,1,2,3,4,5,6,7,31,32,33,34,35,36,37,38,39,310,311,312,313,314]
 # problem = [4,7,1,2,3,5,6]
-problem = [3,4,7]
+#problem = [3,4,7]
 for j in range(len(problem)):
     print('problem', problem[j])
 
@@ -16,9 +16,9 @@ for j in range(len(problem)):
     GPU = '_'.join(GPU[3:])
 
     num = 100
-    iterations = 200
+    iterations = 30
     population = 128
-    pos_dim = 3
+    pos_dim = 200
     f = open('results.pkl', 'wb')
     results = {'count': -1, 'cpu': [], 'gpu': [], 'problem': problem[j],
                'pos_dim': pos_dim, 'gpu2':[]}
